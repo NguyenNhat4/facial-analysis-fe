698e2c23418c162a45683f591aecca34ace7e22a
Nguyễn Minh Nhật <123147485+NguyenNhat4@users.noreply.github.com>
2026-05-28 15:45:17 +0700

    feat: conditionally initialize Firebase analytics for browser environment

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+let analytics;
+// ONLY initialize if we are in the browser
+if (typeof window !== "undefined") {
+    analytics = getAnalytics(app);
+}
 
+export { analytics };
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
