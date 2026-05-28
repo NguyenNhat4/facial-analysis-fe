8ec4e24c81d1061e6c46e9dc2fd0b0e7e6b7ff22
Nguyễn Minh Nhật <123147485+NguyenNhat4@users.noreply.github.com>
2026-05-28 13:54:13 +0700

    feat: remove unused development Dockerfile and demo page test

--- test_demo_page.js
+++
@@ -1,6 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('demo page loads', async ({ page }) => {
-  await page.goto('http://localhost:5173/demo');
-  await page.waitForTimeout(5000);
-});
